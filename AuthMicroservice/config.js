7e84f9534c9a62a38b40d5f3f157b1d60b00a5c6
unknown <janekb1996@gmail.com>
2020-03-29 20:37:25 +0200

    [PC] Change config file

--- AuthMicroservice/config.js
+++ AuthMicroservice/config.js
@@ -1,8 +1,12 @@
 module.exports = {
     "jwtTokenSecretKey": "YOUR MEGA SECRET KEY",
     "expiresIn": "15min",
-    "emailAuth": "EMAIL AUTH PASTE HERE",
     "frontendURL": "http://localhost:3000",
     "cryptographicMicroserviceURL": "http://localhost:4000",
-    "md5Additional": "md5Additional"
+    "authMicroserviceURL": "http://localhost:3999",
+    "emailMicroservice": "http://localhost:3997",
+    "md5Additional": "md5Additional",
+    "expiresInMicroservice": "30s",
+    "microserviceName": "AuthMicroservice",
+    "microservicePassword": "AuthPassword"
 };
