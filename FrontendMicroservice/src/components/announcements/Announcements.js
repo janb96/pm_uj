e0fd57f1e70ee257873360b13ecfcb33dce8a646
unknown <janekb1996@gmail.com>
2020-04-26 23:32:06 +0200

    [NF] Add new component - Announcements - handle page with announcements

---
+++ FrontendMicroservice/src/components/announcements/Announcements.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+//COMPONENTS
+import AnnouncementMiniature from "../announcement/miniature/AnnouncementMiniature";
+
+class Announcements extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            announcements: []
+        };
+    }
+
+    componentDidMount() {
+
+        if(this.props.apiUrl){
+            let apiUrl = this.props.apiUrl;
+            axios.get(apiUrl).then(
+                result => {
+                    let status = result.data.status;
+                    if(status.toString() === "true") {
+                        let announcements = result.data.message;
+                        this.setState({
+                            announcements: announcements
+                        });
+                    }
+                }
+            ).catch(
+                err => {
+                    console.log(err);
+                }
+
+            );
+        }
+
+    }
+
+    render() {
+
+        return(
+          <div className="announcements">
+              <div className="container mt-3">
+                  <h1>Announcements</h1>
+                  <hr/>
+                  {this.state.announcements.map((value, index) => {
+                      return <AnnouncementMiniature announcement={value} key={index.toString()}/>
+                  })}
+              </div>
+          </div>
+        );
+    }
+}
+
+export default Announcements;
