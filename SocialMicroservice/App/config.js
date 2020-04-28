6980e6a539ab2ef90466e4377f9d09e1d0cf4e81
server1 <janekb1996@gmail.com>
2020-04-28 23:09:21 +0200

    [NF] Add Dockerfile to SocialMicroservice + [PC] Add files to App folder and change config

---
+++ SocialMicroservice/App/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "authMicroserviceURL": "http://auth-microservice:3999",
+    "itemsOnPage": 1,
+    "microserviceName": "SocialMicroservice",
+    "microservicePassword": "SocialPassword",
+    "cryptographicMicroserviceURL": "http://cryptographic-microservice:4000",
+    "emailMicroservice": "http://email-microservice:3997",
+};
