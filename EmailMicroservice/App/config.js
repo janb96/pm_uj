c9ed3c8d67822ab2d80b37933366a85651c03841
server1 <janekb1996@gmail.com>
2020-04-28 22:47:56 +0200

    [NF] Add Dockerfile in EmailMicroservice + [PC] Put all files in App directory

---
+++ EmailMicroservice/App/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "emailAuth": "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",
+    "frontendURL": "http://10.0.40.42:3000",
+    "authMicroservice": "http://auth-microservice:3999",
+    "emailMicroservice": "http://email-microservice:3997",
+    "cryptographicMicroserviceURL": "http://cryptographic-microservice:4000"
+};
--- EmailMicroservice/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "emailAuth": "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",
-    "frontendURL": "http://localhost:3000",
-    "authMicroservice": "http://localhost:3999",
-    "emailMicroservice": "http://localhost:3997",
-    "cryptographicMicroserviceURL": "http://localhost:4000"
-};
