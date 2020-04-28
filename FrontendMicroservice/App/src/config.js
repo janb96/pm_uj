b6b50161129d048bcf961af033d308c4f8ad60b6
server1 <janekb1996@gmail.com>
2020-04-28 23:03:14 +0200

    [NF] Add Dockerfile to FrontendMicroservice + [PC] Add all files to App directory and change config

---
+++ FrontendMicroservice/App/src/config.js
@@ -0,0 +1,6 @@
+export const config = {
+    "authMicroservice": "http://10.0.40.42:3999",
+    "fileMicroservice": "http://10.0.40.42:3996",
+    "productMicroservice": "http://10.0.40.42:3998",
+    "socialMicroservice": "http://10.0.40.42:4001"
+};
