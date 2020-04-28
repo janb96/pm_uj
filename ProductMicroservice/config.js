7d01d9421cc559f425b54d76390f554ce70ddf39
server1 <janekb1996@gmail.com>
2020-04-28 23:07:46 +0200

    [NF] Add Dockerfile to ProductMicroservice + [PC] Add files to App directory and change config

--- ProductMicroservice/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "authMicroserviceURL": "http://localhost:3999",
-    "itemsOnPage": 4,
-    "microserviceName": "ProductMicroservice",
-    "microservicePassword": "ProductPassword",
-    "cryptographicMicroserviceURL": "http://localhost:4000",
-    "emailMicroservice": "http://localhost:3997",
-};
